909f4c512296737fe7925b5ecd05e4a5aaf487e4
Madhukar Subedi <madhukar@excelthemes.com>
2016-07-13 17:57:51 +0545

    added files

---
+++ assets/js/scripts.js
@@ -0,0 +1,85 @@
+!function (e) {
+    "use strict";
+    var n = window.AK_JS || {};
+        
+        n.PreLoader = function () {
+            e(".page-loader").hide();
+        },
+
+        n.mobileMenu = {
+            init: function () {
+                this.toggleMenu(), this.addClassParent(), this.addRemoveClasses()
+            },
+            toggleMenu: function () {
+                var n = this,
+                    t = e("#masthead");
+                e("#nav-toggle").click(function (a) {
+                    a.preventDefault(), t.hasClass("open") ? (t.removeClass("open"), e(".menu").find("li").removeClass("show")) : (t.addClass("open"), n.showSubmenu())
+                })
+            },
+            addClassParent: function () {
+                e(".menu").find("li > ul").each(function () {
+                    e(this).parent().addClass("parent")
+                })
+            },
+            addRemoveClasses: function () {
+                var n = e(".menu");
+                e(window).width() < 992 ? n.addClass("mobile") : (e("body").removeClass("open"), n.removeClass("mobile").find("li").removeClass("show"))
+            },
+            showSubmenu: function () {
+                e(".menu").find("a").each(function () {
+                    var n = e(this);
+                    n.next("ul").length && n.one("click", function (n) {
+                        n.preventDefault(), e(this).parent().addClass("show")
+                    })
+                })
+            }
+        },
+
+        n.DataBackground = function () {
+            var pageSection = e(".data-bg");
+            pageSection.each(function (indx) {
+
+                if (e(this).attr("data-background")) {
+                    e(this).css("background-image", "url(" + e(this).data("background") + ")");
+                }
+            });
+        },
+
+
+        e(document).ready(function () {
+            n.PreLoader(), n.mobileMenu.init(), n.DataBackground()
+        }), e(window).resize(function () {
+        n.mobileMenu.addRemoveClasses()
+    })
+}(jQuery);
+
+//Morris charts snippet - js
+
+jQuery(document).ready(function ($) {
+  Morris.Donut({
+        element: 'donut-example',
+        data: [
+         {label: "Active Lerners", value: 63},
+         {label: "Non Active", value: 37}
+        ]
+      });
+
+  Morris.Bar({
+  element: 'bar-example',
+  data: [
+    { y: '2/15', a: 5},
+    { y: '2/16', a: 4},
+    { y: '2/17', a: 9},
+    { y: '2/18', a: 10},
+    { y: '2/19', a: 15},
+    { y: '2/20', a: 10},
+    { y: '2/21', a: 14}
+  ],
+  xkey: 'y',
+  ykeys: ['a'],
+  labels: ['Series A', 'Series B']
+});
+
+
+});
